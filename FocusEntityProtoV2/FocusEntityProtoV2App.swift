6d0bfda5881c4f1b583b871e2db943308ec2138b
Nahom <ghebredn@msu.edu>
2023-09-10 19:06:42 -0400

    Initial Commit

---
+++ FocusEntityProtoV2/FocusEntityProtoV2App.swift
@@ -0,0 +1,17 @@
+//
+//  FocusEntityProtoV2App.swift
+//  FocusEntityProtoV2
+//
+//  Created by cse498 on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusEntityProtoV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
